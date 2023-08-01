c7feb1b59e00deab879f6f19bcd2a7e8e6ed5f2a
KurtisIvey <kurtiveycodes@gmail.com>
2023-07-31 23:19:11 -0500

    add pseudocode

---
+++ 07-31-2023/addLength.js
@@ -0,0 +1,7 @@
+/**
+ p: take in string
+ r: return string split into arrays with space after word with the length
+ ep: split the string via " "
+    then iterate through, get the length and add " ${.length}"
+    return the array
+ */
