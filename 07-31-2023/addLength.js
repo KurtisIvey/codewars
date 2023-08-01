b990c628f097c209e5372ad0e581302560be6a46
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-01 00:47:31 -0500

    update

--- 07-31-2023/addLength.js
+++ 07-31-2023/addLength.js
@@ -5,3 +5,33 @@
     then iterate through, get the length and add " ${.length}"
     return the array
  */
+
+function addLength(str) {
+  let strArr = str.split(" ");
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i] += ` ${strArr[i].length}`;
+  }
+  return strArr;
+}
+
+// less code:
+function addLength(str) {
+  return str.split(" ").map((s) => `${s} ${s.length}`);
+}
+
+function addLength(str) {
+  // Step 1: Split the input string into an array of words using the space (" ") as the delimiter.
+  // For example, if str = "Hello world", the array will be ["Hello", "world"].
+  const wordsArray = str.split(" ");
+
+  // Step 2: Use the map() method to iterate through each element (word) of the wordsArray.
+  // For each word, we concatenate it with a space and its length to form a new string.
+  // The map() method returns a new array with the modified strings.
+  // For example, if the input was "Hello world", the mapped array will be ["Hello 5", "world 5"].
+  const mappedArray = wordsArray.map((s) => `${s} ${s.length}`);
+
+  // Step 3: Return the mappedArray, which now contains the original words from the input string,
+  // concatenated with a space and their respective lengths.
+  // For example, if the input was "Hello world", the output will be ["Hello 5", "world 5"].
+  return mappedArray;
+}
