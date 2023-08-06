ac6eba07546e780f7823aaa1b1e50658d5d7b972
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-05 23:54:18 -0500

    update

--- 08-05-2023/removeEveryOther.js
+++ 08-05-2023/removeEveryOther.js
@@ -3,5 +3,6 @@ function removeEveryOther(arr){
         if(index %2 === 0){
             return item
         }
+        // must attach filter as index %2 != 0 is returned undefined
     }).filter((item) => item !== undefined)
 }
