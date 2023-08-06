3dffe6e4d36c4a65015e9edac55cb897cad6ff84
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-05 23:59:42 -0500

    update

---
+++ 08-05-2023/stringy.js
@@ -0,0 +1,10 @@
+function stringy(size){
+    let output = '';
+    for(let i = 1;i <  + 1 ;i++){
+        if(i % 2 !== 0){
+            output += '1'
+        }else{
+            output += '0'
+        }
+    }return output
+}
