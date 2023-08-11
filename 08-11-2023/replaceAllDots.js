bafae047646c73b172457561d1bb05c27307d661
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-11 16:54:58 -0500

    update

---
+++ 08-11-2023/replaceAllDots.js
@@ -0,0 +1,9 @@
+// original, only replaces first occurence
+var replaceDots = function (str) {
+  return str.replace(".", "-");
+};
+
+var replaceDots = function (str) {
+  // add all to change to replaceAll built in function
+  return str.replaceAll(".", "-");
+};
