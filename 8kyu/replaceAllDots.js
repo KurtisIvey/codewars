a301cff6ba0715148f131fad068cbefb837ef914
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-11 17:04:19 -0500

    update

--- 8kyu/replaceAllDots.js
+++ 8kyu/replaceAllDots.js
@@ -3,7 +3,11 @@ var replaceDots = function (str) {
   return str.replace(".", "-");
 };
 
+// solution with replaceAll
 var replaceDots = function (str) {
   // add all to change to replaceAll built in function
   return str.replaceAll(".", "-");
 };
+
+// alt solution with regex
+return str.replace(/\./g, "-");
